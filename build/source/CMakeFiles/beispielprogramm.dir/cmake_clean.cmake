7d825dd917dc439f5551020ff2a0093f6fd4bc53
LS060598 <laura.simon@uni-weimar.de>
2017-06-07 10:15:24 +0200

    alles ausführbar

---
+++ build/source/CMakeFiles/beispielprogramm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beispielprogramm.dir/beispielprogramm.cpp.o"
+  "beispielprogramm.pdb"
+  "beispielprogramm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/beispielprogramm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
